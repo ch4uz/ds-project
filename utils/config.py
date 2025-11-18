76a5b54e0f06ada15b75d6e356b86c8abce3bad0
Åmund <aamgrim@gmail.com>
2025-11-18 22:48:58 +0000

    fix paths

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,7 @@ from cycler import cycler
 
 # _________________________________________ STYLE ____________________________________________________
 simplefilter("ignore")
-style.use("dslabs.mplstyle")
+style.use("../../../utils/dslabs.mplstyle")
 
 HATCHES = [".", "..", "...", "o"]  # ['/', '+', 'X', '*'] #'oo', 'OO', '..'
 
